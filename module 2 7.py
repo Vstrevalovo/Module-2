e5f5ec38af7c725b12cc3580fc702d030bf958dc
Vstrevalovo <berejan97@mail.ru>
2024-06-13 16:16:11 +0300

    Add files via upload

---
+++ module 2 7.py
@@ -0,0 +1,16 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+print_params()
+print_params(123)
+print_params('axe', 'sword')
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+
+values_list = [777, 'три топора', False]
+values_dict = {'a': 'первое значение', 'b': 2, 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
